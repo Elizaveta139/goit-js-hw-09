ffde8e8306e00c0f4f4c18e2c717dca6096e7616
Elizaveta139 <139202899+Elizaveta139@users.noreply.github.com>
2023-11-20 21:04:18 +0000

    Deploying to gh-pages from @ Elizaveta139/goit-js-hw-09@a748bef7badbea9cf406c1eaf181853341e07378 🚀

---
+++ 01-color-switcher.d8d13906.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(t){l(),d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),console.log("Click start")})),e.addEventListener("click",(function(t){clearInterval(d),console.log("Click stop"),l()})),e.disabled=!0;let d=null;function l(){t.disabled?(t.disabled=!1,e.disabled=!0):(t.disabled=!0,e.disabled=!1)}
+//# sourceMappingURL=01-color-switcher.d8d13906.js.map
